855fa68e3ad2afbd8c49fd6d7d9f968f5769656b
Gubarev <mg@sendpulse.com>
2023-03-07 18:35:09 +0200

    Add css

--- laravel/app/Http/Controllers/Frontend/IndexController.php
+++ laravel/app/Http/Controllers/Frontend/IndexController.php
@@ -11,31 +11,16 @@ class IndexController
 {
     public function index(FormRequest $request, ItemsRepository $itemsRepository)
     {
-        $redis = Redis::connection()->client();
-        echo "Connection to server sucessfully <br>";
-        $redis->set("tutorial-name", "Redis tutorial");
-        echo "Stored string in redis:: " . $redis->get("tutorial-name") . '<br>';
-
-        $item = $itemsRepository->getById(1);
-        dd($item->content);
-
         /**
-        $redis = new \Redis();
-        $redis->connect('redis', 6379);
-        echo "Connection to server sucessfully <br>";
-        $redis->set("tutorial-name", "Redis tutorial");
-        echo "Stored string in redis:: " . $redis->get("tutorial-name") . '<br>';
-
-        $server = 'mysql';
-        $username = 'root';
-        $password = 'root';
-
-        $mysql = new \MySQLi($server, $username, $password, 'blog');
-        if ($mysql->connect_errno) {
-            echo "Errno: " . $mysql->connect_errno . "<br>";
-        }
-        $result = $mysql -> query("SELECT * FROM test");
-        print_r($result);
+         * $redis = Redis::connection()->client();
+         * echo "Connection to server sucessfully <br>";
+         * $redis->set("tutorial-name", "Redis tutorial");
+         * echo "Stored string in redis:: " . $redis->get("tutorial-name") . '<br>';
+         *
+         * $item = $itemsRepository->getById(1);
+         * dd($item->content);
          */
+
+        return view('items/index');
     }
 }
