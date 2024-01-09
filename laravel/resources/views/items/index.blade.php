ead55078f1707294a4940ee0e0d7f46261087938
Gubarev <tutsan8@ukr.net>
2024-01-09 23:53:20 +0200

    Search by content

--- laravel/resources/views/items/index.blade.php
+++ laravel/resources/views/items/index.blade.php
@@ -4,80 +4,93 @@
     <div class="row">
         <!--blog start-->
         <div class="col-lg-9">
-            <div class="blog-item">
-                @foreach ($items['list'] as $item)
-                    <div class="row">
-                        <div class="col-lg-2 col-sm-2 text-right">
-                            <div class="date-wrap">
+            @if (empty($items['list']))
+                <div class="blog-item">
+                    <h3>
+                        На ваш запит нічого не знайдено.
+                        Можливо, варто спробувати більш загальні чи специфічні ключові слова для пошуку
+                    </h3>
+                </div>
+            @else
+                <div class="blog-item">
+                    @foreach ($items['list'] as $item)
+                        <div class="row">
+                            <div class="col-lg-2 col-sm-2 text-right">
+                                <div class="date-wrap">
                                   <span class="date">
                                       <?= date_parse($item['created'])['day'] ?>
                                   </span>
-                                <span class="month">
+                                    <span class="month">
                                       <?= getUkrainianMonth($item['created']) ?>
                                 </span>
-                            </div>
-                            <ul class="list-unstyled">
-                                <li style="padding-bottom: 1px">
-                                    <em>
-                                        209 View
-                                    </em>
-                                </li>
-                                <li class="hidden">
-                                    <em>
-                                        23 Share
-                                    </em>
-                                </li>
-                            </ul>
-                            <div class="st-view hidden" style="margin-top: 0px">
+                                </div>
                                 <ul class="list-unstyled">
-                                    <li>
-                                        <a href="javascript:;">
-                                            15 comments
-                                        </a>
+                                    <li style="padding-bottom: 1px">
+                                        <em>
+                                            209 View
+                                        </em>
+                                    </li>
+                                    <li class="hidden">
+                                        <em>
+                                            23 Share
+                                        </em>
                                     </li>
                                 </ul>
+                                <div class="st-view hidden" style="margin-top: 0px">
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <a href="javascript:;">
+                                                15 comments
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-10 col-sm-10">
+                                    <?php /** ?>
+                                 * <h1>
+                                 * <a href="blog-detail.html">
+                                 * Suspendisse dignissim in sem eget pulvinar. Mauris aliquam nulla at libero pretium.
+                                 * </a>
+                                 * </h1>
+                                 * <?php */ ?>
+                                <h3 style="white-space: pre-line; margin-top:-15px; margin-bottom: 15px; color: #444e67; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif">
+                                    {{ Str::ucfirst($item['content']) }}
+                                </h3>
+                                <br>
+                                <ul class="tag">
+                                    @foreach ($item->getTags() as $tag)
+                                        <li>
+                                            <a href="/?tag={{ $tag->id }}" style="color: #797979">
+                                                <i class="fa fa-tags pr-5"></i>
+                                                {{ $tag->name }}
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                                {{--                            <a href="blog-detail.html" class="btn btn-primary">--}}
+                                {{--                                Continue Reading--}}
+                                {{--                            </a>--}}
                             </div>
                         </div>
-                        <div class="col-lg-10 col-sm-10">
-                                <?php /** ?>
-                             * <h1>
-                             * <a href="blog-detail.html">
-                             * Suspendisse dignissim in sem eget pulvinar. Mauris aliquam nulla at libero pretium.
-                             * </a>
-                             * </h1>
-                             * <?php */ ?>
-                            <h3 style="white-space: pre-line; margin-top:-15px; margin-bottom: 15px; color: #444e67; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif">
-                                {{ Str::ucfirst($item['content']) }}
-                            </h3>
-                            <br>
-                            <ul class="tag">
-                                @foreach ($item->getTags() as $tag)
-                                    <li>
-                                        <a href="/?tag={{ $tag->id }}" style="color: #797979">
-                                            <i class="fa fa-tags pr-5"></i>
-                                            {{ $tag->name }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                            {{--                            <a href="blog-detail.html" class="btn btn-primary">--}}
-                            {{--                                Continue Reading--}}
-                            {{--                            </a>--}}
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+                    @endforeach
+                </div>
 
-            <div class="text-center">
-                <?php echo buildPagination($items['total_pages'], $items['current_page']); ?>
-            </div>
+                <div class="text-center">
+                        <?php echo buildPagination($items['total_pages'], $items['current_page']); ?>
+                </div>
+            @endif
         </div>
 
         <div class="col-lg-3">
             <div class="blog-side-item">
                 <div class="search-row">
                     <form action="/" method="GET" class="search-form">
-                        <input type="text" name="search" class="form-control" placeholder="Шукати тут" onkeypress="handleSearchKeyPress(event, this)">
+                        <input type="text" name="search" class="form-control"
+                               placeholder="Шукати тут"
+                               onkeypress="handleSearchKeyPress(event, this)"
+                               value="{{ request()->input('search') }}"
+                        >
                     </form>
                 </div>
                 <div class="category">
