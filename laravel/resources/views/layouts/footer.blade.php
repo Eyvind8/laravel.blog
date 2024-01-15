12f2b2d59b8bf4217094d861e466e00d8a9799d7
Gubarev <tutsan8@ukr.net>
2024-01-15 07:41:05 +0200

    Change footer

--- laravel/resources/views/layouts/footer.blade.php
+++ laravel/resources/views/layouts/footer.blade.php
@@ -3,150 +3,172 @@
         <div class="row">
             <div class="col-lg-3 col-sm-3 address wow fadeInUp" data-wow-duration="2s" data-wow-delay=".1s">
                 <h1>
-                    contact info
+                    наші контакти
                 </h1>
                 <address>
-                    <p>
-                        <i class="fa fa-home pr-10">
-                        </i>
-                        Address: No.XXXXXX street
-                    </p>
+{{--                    <p>--}}
+{{--                        <i class="fa fa-home pr-10">--}}
+{{--                        </i>--}}
+{{--                        Address: No.XXXXXX street--}}
+{{--                    </p>--}}
                     <p>
                         <i class="fa fa-globe pr-10">
                         </i>
-                        Mars city, Country
+                        Чернігів, Україна
                     </p>
-                    <p>
-                        <i class="fa fa-mobile pr-10">
-                        </i>
-                        Mobile : (123) 456-7890
-                    </p>
+{{--                    <p>--}}
+{{--                        <i class="fa fa-mobile pr-10">--}}
+{{--                        </i>--}}
+{{--                        Mobile : (123) 456-7890--}}
+{{--                    </p>--}}
                     <p>
                         <i class="fa fa-phone pr-10">
                         </i>
-                        Phone : (123) 456-7890
+                        Phone : (095) 734-0687
                     </p>
                     <p>
                         <i class="fa fa-envelope pr-10">
                         </i>
                         Email :
-                        <a href="javascript:;">
-                            support@example.com
-                        </a>
+                        <a href="mailto:support@joks.club">support@joks.club</a>
                     </p>
                 </address>
             </div>
-            <div class="col-lg-3 col-sm-3 wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">
-                <h1>
-                    latest tweet
-                </h1>
-                <div id="owl-slide">
-                    <div class="tweet-box">
-                        <i class="fa fa-twitter">
-                        </i>
-                        <em>
-                            Please follow
-                            <a href="javascript:;">
-                                @example
-                            </a>
-                            for all future updates of us!
-                            <a href="javascript:;">
-                                twitter.com/acme
-                            </a>
-                        </em>
-                    </div>
-                    <div class="tweet-box">
-                        <i class="fa fa-twitter">
-                        </i>
-                        <em>
-                            Please follow
-                            <a href="javascript:;">
-                                @example
-                            </a>
-                            for all future updates of us!
-                            <a href="javascript:;">
-                                twitter.com/acme
-                            </a>
-                        </em>
-                    </div>
-                    <div class="tweet-box">
-                        <i class="fa fa-twitter">
-                        </i>
-                        <em>
-                            Please follow
-                            <a href="javascript:;">
-                                @example
-                            </a>
-                            for all future updates of us!
-                            <a href="javascript:;">
-                                twitter.com/acme
-                            </a>
-                        </em>
-                    </div>
-                </div>
-            </div>
+{{--            <div class="col-lg-3 col-sm-3 wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">--}}
+{{--                <h1>--}}
+{{--                    latest tweet--}}
+{{--                </h1>--}}
+{{--                <div id="owl-slide">--}}
+{{--                    <div class="tweet-box">--}}
+{{--                        <i class="fa fa-twitter">--}}
+{{--                        </i>--}}
+{{--                        <em>--}}
+{{--                            Please follow--}}
+{{--                            <a href="javascript:;">--}}
+{{--                                @example--}}
+{{--                            </a>--}}
+{{--                            for all future updates of us!--}}
+{{--                            <a href="javascript:;">--}}
+{{--                                twitter.com/acme--}}
+{{--                            </a>--}}
+{{--                        </em>--}}
+{{--                    </div>--}}
+{{--                    <div class="tweet-box">--}}
+{{--                        <i class="fa fa-twitter">--}}
+{{--                        </i>--}}
+{{--                        <em>--}}
+{{--                            Please follow--}}
+{{--                            <a href="javascript:;">--}}
+{{--                                @example--}}
+{{--                            </a>--}}
+{{--                            for all future updates of us!--}}
+{{--                            <a href="javascript:;">--}}
+{{--                                twitter.com/acme--}}
+{{--                            </a>--}}
+{{--                        </em>--}}
+{{--                    </div>--}}
+{{--                    <div class="tweet-box">--}}
+{{--                        <i class="fa fa-twitter">--}}
+{{--                        </i>--}}
+{{--                        <em>--}}
+{{--                            Please follow--}}
+{{--                            <a href="javascript:;">--}}
+{{--                                @example--}}
+{{--                            </a>--}}
+{{--                            for all future updates of us!--}}
+{{--                            <a href="javascript:;">--}}
+{{--                                twitter.com/acme--}}
+{{--                            </a>--}}
+{{--                        </em>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="col-lg-3 col-sm-3">--}}
+{{--                <div class="page-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".5s">--}}
+{{--                    <h1>--}}
+{{--                        Our Company--}}
+{{--                    </h1>--}}
+{{--                    <ul class="page-footer-list">--}}
+
+{{--                        <li>--}}
+{{--                            <i class="fa fa-angle-right">--}}
+{{--                            </i>--}}
+{{--                            <a href="about.html">--}}
+{{--                                About Us--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+{{--                        <li>--}}
+{{--                            <i class="fa fa-angle-right">--}}
+{{--                            </i>--}}
+{{--                            <a href="faq.html">--}}
+{{--                                Support--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+{{--                        <li>--}}
+{{--                            <i class="fa fa-angle-right">--}}
+{{--                            </i>--}}
+{{--                            <a href="service.html">--}}
+{{--                                Service--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+{{--                        <li>--}}
+{{--                            <i class="fa fa-angle-right">--}}
+{{--                            </i>--}}
+{{--                            <a href="privacy-policy.html">--}}
+{{--                                Privacy Policy--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+{{--                        <li>--}}
+{{--                            <i class="fa fa-angle-right">--}}
+{{--                            </i>--}}
+{{--                            <a href="career.html">--}}
+{{--                                We are Hiring--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+{{--                        <li>--}}
+{{--                            <i class="fa fa-angle-right">--}}
+{{--                            </i>--}}
+{{--                            <a href="terms.html">--}}
+{{--                                Term & condition--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
+
+{{--                    </ul>--}}
+{{--                </div>--}}
+{{--            </div>--}}
             <div class="col-lg-3 col-sm-3">
-                <div class="page-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".5s">
+
+                <div class="text-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".7s">
                     <h1>
-                        Our Company
+                        Смішність на вибір
                     </h1>
-                    <ul class="page-footer-list">
-
-                        <li>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            <a href="about.html">
-                                About Us
-                            </a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            <a href="faq.html">
-                                Support
-                            </a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            <a href="service.html">
-                                Service
-                            </a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            <a href="privacy-policy.html">
-                                Privacy Policy
-                            </a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            <a href="career.html">
-                                We are Hiring
-                            </a>
-                        </li>
-                        <li>
-                            <i class="fa fa-angle-right">
-                            </i>
-                            <a href="terms.html">
-                                Term & condition
-                            </a>
-                        </li>
-
-                    </ul>
+                    <p>
+                        Ваш особистий антидепресант!
+                        Тут кожен жарт – наче чарівна таблетка щастя, готова миттєво підняти ваш настрій
+                    </p>
                 </div>
             </div>
             <div class="col-lg-3 col-sm-3">
 
                 <div class="text-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".7s">
                     <h1>
-                        Text Widget
+                        Веселий контент щодня
                     </h1>
                     <p>
-                        This is a text widget.Lorem ipsum dolor sit amet.
-                        This is a text widget.Lorem ipsum dolor sit amet
+                        Забудьте про сірі будні – у нас завжди світло і весело!
+                        Кожен текст – це крок у світ гарного настрою
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-3">
+
+                <div class="text-footer wow fadeInUp" data-wow-duration="2s" data-wow-delay=".7s">
+                    <h1>
+                        Настрій гарантовано
+                    </h1>
+                    <p>
+                        Ласкаво просимо на наш сайт, де гумор – наше головне завдання!
+                        Тут кожен жарт – наче чарівна таблетка щастя, готова миттєво підняти ваш настрій
                     </p>
                 </div>
             </div>
@@ -162,27 +184,20 @@
         <div class="row">
             <div class="col-lg-6 col-sm-6 pull-right">
                 <ul class="social-link-footer list-unstyled">
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".1s"><a href="#"><i
-                                class="fa fa-facebook"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".2s"><a href="#"><i
-                                class="fa fa-google-plus"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".3s"><a href="#"><i
-                                class="fa fa-dribbble"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".4s"><a href="#"><i
-                                class="fa fa-linkedin"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".5s"><a href="#"><i
-                                class="fa fa-twitter"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".6s"><a href="#"><i
-                                class="fa fa-skype"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".7s"><a href="#"><i
-                                class="fa fa-github"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".8s"><a href="#"><i
-                                class="fa fa-youtube"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".1s"><a href="https://www.facebook.com/sharer.php?u=https%3A%2F%2Fжарт.club" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".2s"><a href="https://plus.google.com/share?url=https%3A%2F%2Fжарт.club" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".3s"><a href="https://www.linkedin.com/shareArticle?url=https%3A%2F%2Fжарт.club" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".4s"><a href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fжарт.club&text=Заголовок%20сторінки%20" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".5s"><a href="skype:жарт.club?chat" target="_blank"><i class="fa fa-skype"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".6s"><a href="https://github.com/жарт-club" target="_blank"><i class="fa fa-github"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".7s"><a href="https://www.youtube.com/share?v=https%3A%2F%2Fжарт.club" target="_blank"><i class="fa fa-youtube"></i></a></li>
                 </ul>
+
+
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <div class="copyright">
-                    <p>&copy; Copyright - Acme Theme by cosmic.</p>
+                    <p>© 2024 Копіювання з зазначенням посилання дозволено</p>
                 </div>
             </div>
         </div>
