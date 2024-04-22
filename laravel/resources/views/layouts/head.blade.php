327092d0a2a6fa92533cc5249e1da30d1394230d
Gubarev <tutsan8@ukr.net>
2024-04-22 13:08:56 +0300

    Add favicon

--- laravel/resources/views/layouts/head.blade.php
+++ laravel/resources/views/layouts/head.blade.php
@@ -1,5 +1,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-{{--<link rel="shortcut icon" href="img/favicon.png">--}}
+{{--<link rel="shortcut icon" href="/img/favicon/favicon.png">--}}
+<link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+<link rel="manifest" href="/img/favicon/site.webmanifest">
 
 
 @if (strpos(request()->url(), '/id/') !== false)
