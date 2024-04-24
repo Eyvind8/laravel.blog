68885a23c5d415f6f2ca6dc17acef8166dd15b35
Gubarev <tutsan8@ukr.net>
2024-04-24 15:08:43 +0300

    Add google tag

--- laravel/resources/views/layouts/head.blade.php
+++ laravel/resources/views/layouts/head.blade.php
@@ -91,3 +91,15 @@
 <![endif]-->
 
 <meta name="csrf-token" content="{{ csrf_token() }}">
+
+@if (env('APP_ENV') === 'prod')
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-10Q3SDJ36W"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-10Q3SDJ36W');
+    </script>
+@endif
